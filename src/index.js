8dde081a6f7d722ec5b32ea81bff63c6bc2e1283
tbirchem <taylor.birhem@icloud.com>
2022-07-11 23:13:56 -0700

    Test push

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ ReactDOM.render(
         <Route path="/index" render={(props) => <Index {...props} />} />
         <Route
           path="/nucleo-icons"
-          render={(props) => <NucleoIcons {...props} />}
+           render={(props) => <NucleoIcons {...props} />}
         />
         <Route
           path="/landing-page"
